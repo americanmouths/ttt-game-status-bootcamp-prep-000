71bbb4996d33e96bf657c40a6a45ad4b084d791a
Kayla Hansen <shiverr@gmail.com>
2017-09-23 22:37:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,15 +16,11 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
-    if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
-      return win_combo
-    elsif board[win_combo[0]] == "O" && board[win_combo[2]] == "O" && board[win_combo[2]] == "O"
-      return win_combo
-    else
-    end
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
-  false
 end
 
 def full?(board)
